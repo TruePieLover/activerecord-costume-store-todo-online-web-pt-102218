deda41346e7f05236cdfea73e92693e6c34eec9e
Brandon Teich <brandonteich123@gmail.com>
2019-03-03 19:35:47 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,15 +1,17 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[5.0]
+  
   def change
-    create_table :haunted_houses do |t|
-      t.string :name
-      t.string :location
-      t.string :theme
-      t.integer :price
-      t.boolean :family_friendly
-      t.date :opening_date
-      t.date :closing_date
-      t.text :description
+    create_table :haunted_houses do |x|
+      x.string :name
+      x.string :location
+      x.string :theme
+      x.integer :price
+      x.boolean :family_friendly
+      x.date :opening_date
+      x.date :closing_date
+      x.text :description
     end
   end
+  
 end 
